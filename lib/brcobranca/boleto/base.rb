b467e96f43df1f3ead5b1c115454213c1580c82f
Londerson <londerson@gmail.com>
2014-07-28 18:49:15 -0300

    change text in Local field

--- lib/brcobranca/boleto/base.rb
+++ lib/brcobranca/boleto/base.rb
@@ -82,7 +82,7 @@ module Brcobranca
         padrao = {
           :moeda => "9", :data_documento => Date.today, :dias_vencimento => 1, :quantidade => 1,
           :especie_documento => "DM", :especie => "R$", :aceite => "S", :valor => 0.0,
-          :local_pagamento => "QUALQUER BANCO ATÉ O VENCIMENTO"
+          :local_pagamento => "Até o vencimento, preferencialmente no Itaú"
         }
 
         campos = padrao.merge!(campos)
